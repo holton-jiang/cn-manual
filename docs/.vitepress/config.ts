f1873c6df3dee916a6c0abfa525e874dfe5b963e
蒋小霕 <Holton.Jiang@Gmail.com>
2023-06-01 15:29:21 +0800

    Update config.ts
    Signed-off-by: 蒋小霕 <Holton.Jiang@Gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,9 +24,9 @@ export default defineConfig({
           '我们怎么到这来了？',
           '这是一个 404 页面',
           '看起来我们进入了错误的链接',
+          '但是，如果你持之以恒，继续探寻，你可能会最终抵达你要去的地方。',
         ],
         backToHome: '返回首页',
-        // a11y
         openInNewWindow: '在新窗口打开',
         toggleColorMode: '切换颜色模式',
         toggleSidebar: '切换侧边栏',
