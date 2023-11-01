e9ffdaf7dde25f40711299485a09d20f1662a5b2
蒋小霕 <Holton.Jiang@Gmail.com>
2023-11-01 13:35:12 +0800

    Update config.ts
    Signed-off-by: 蒋小霕 <Holton.Jiang@Gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -39,17 +39,21 @@ export default defineConfig({
   search: {
 			provider: 'local',
 			options: {
-				translations: {
-					button: {
-						buttonText: '搜索文档',
-						buttonAriaLabel: '搜索文档',
-					},
-					modal: {
-						noResultsText: '无法找到相关结果',
-						resetButtonTitle: '清除查询条件',
-						footer: {
-							selectText: '选择',
-							navigateText: '切换',
+				locales: {
+					zh: {
+						translations: {
+							button: {
+								buttonText: '搜索文档',
+								buttonAriaLabel: '搜索文档',
+							},
+							modal: {
+								noResultsText: '无法找到相关结果',
+								resetButtonTitle: '清除查询条件',
+								footer: {
+									selectText: '选择',
+									navigateText: '切换',
+								},
+							},
 						},
 					},
 				},
