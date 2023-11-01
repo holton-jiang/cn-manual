0c8fa84b0d141f4c5d60fe07a71b091a502c3912
蒋小霕 <Holton.Jiang@Gmail.com>
2023-11-01 13:26:23 +0800

    Add files via upload
    Signed-off-by: 蒋小霕 <Holton.Jiang@Gmail.com>

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,13 @@
+import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import './custom.css'
+import Layout from './Layout.vue'
+
+export default {
+	...DefaultTheme,
+	Layout() {
+		return h(Layout, null, {
+			'doc-before': () => h(AdComponent),
+		})
+	},
+}
